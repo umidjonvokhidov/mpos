29d46361360b47eaba07137735b04cddf70254bb
Umidjon Vokhidov <umidjonvokhidov.uz@gmail.com>
2025-08-22 23:22:21 +0500

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import type { Metadata } from 'next';
 import { useAuth, useCart, useNotification, useProduct, useUI } from '@/stores';
 import Loading from '../loading';
 import { useRouter } from 'next/navigation';
@@ -8,12 +7,6 @@ import { useEffect } from 'react';
 import { toast } from 'sonner';
 import React from 'react';
 import Navbar from '@/components/Navbar';
-
-export const metadata: Metadata = {
-  title: 'MPos',
-  description:
-    'A modern, user-friendly point of sale (POS) system for efficient product management, sales tracking, and business reporting.',
-};
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   const router = useRouter();
